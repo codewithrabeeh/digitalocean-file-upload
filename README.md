e286e2427a093289821757fb42eb294a781da5f8
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-07-27 20:06:41 +0530

    init: first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Upload files to Digital Ocean Spaces using NodeJS
+
+[Project Reference 1](https://medium.com/@98.hassanmehdi/uploading-files-to-digital-ocean-spaces-nodejs-1a080da7c45b) 
+
+[Project Reference 2](https://javascript.plainenglish.io/file-uploads-with-digitalocean-spaces-1d35742f1b5c)
---
+++ app.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+const AWS = require('aws-sdk');
+const fs = require('fs');
+
+// Set your DigitalOcean Spaces keys
+const spacesEndpoint = new AWS.Endpoint(process.env.DO_SPACES_ENDPOINT); 
+const spacesAccessKeyId = process.env.DO_SPACES_KEY; // Space Access Key
+const spacesSecretAccessKey = process.env.DO_SPACES_SECRET; // Secret Key of Space Access Key
+const bucketName = process.env.DO_SPACES_NAME; // Bucket Name
+
+// Set up AWS configuration
+const s3 = new AWS.S3({
+  endpoint: spacesEndpoint,
+  accessKeyId: spacesAccessKeyId,
+  secretAccessKey: spacesSecretAccessKey,
+});
+
+// File path of the image you want to upload
+const filePath = 'cat.jpg'; // Replace with the actual file path
+
+// Read the file data
+const fileContent = fs.readFileSync(filePath);
+
+// Set the parameters for the object upload
+const params = {
+  Bucket: bucketName,
+  Key: 'cat.jpg', // The name you want to give to the object in the bucket
+  Body: fileContent,
+  ACL: 'public-read', // Make the uploaded file publicly accessible
+};
+
+// Upload the object to the bucket
+s3.upload(params, (err, data) => {
+  if (err) {
+    console.error('Error uploading the file:', err);
+  } else {
+    console.log('File uploaded successfully. Public URL:', data.Location);
+  }
+});
